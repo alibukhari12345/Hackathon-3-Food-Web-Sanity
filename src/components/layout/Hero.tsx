6c85a9f7777422396ac67390eeaf6d870ca75261
unknown <bshyder123@gmail.com>
2025-01-24 22:11:42 +0500

    done

--- src/components/layout/Hero.tsx
+++ src/components/layout/Hero.tsx
@@ -8,15 +8,11 @@ import { FaPinterestP } from "react-icons/fa6";
 const Hero = () => {
   return (
     <div className='lg:flex lg:gap-[100px] lg:px-[100px] px-6 lg:pt-[101px] pt-[50px]' >
-      <div className='lg:flex flex-col lg:gap-[100px]  gap-4 lg:w-[25.28px] lg:h-[492px] md:hidden sm:hidden hidden'> 
-        <div className='lg:w-[108px] border-[1px] rotate-90 text-whitetext lg:block hidden'></div>
-       <div className='text-[] lg:ml-[45px] flex-col justify-between flex gap-[20px] '>
-      < FaFacebookF className='text-whitetext' />
-        <FaTwitter className='text-bordercoloryello '/>
-        <FaPinterestP className='text-whitetext'/>
-       </div>
-        <div className='w-[108px] border-[1px] lg:block hidden rotate-90 text-whitetext'></div>
-      </div>
+      {/* <div className='lg:flex flex-col lg:gap-[100px]  gap-4 lg:w-[25.28px] lg:h-[492px] md:hidden sm:hidden hidden'> 
+   
+   
+      
+      </div> */}
       <div className='lg:w-[472px] w-[300px] lg:h-[356px] rounded-[30px] flex flex-col lg:gap-[20px] gap-[25px] '>
         <h1 className='text-bordercoloryello font-greatVibes lg:text-[32px] text-[30px] leading-[40px] font-medium '>Its Quick & Amusing!</h1>
         <h1 className='font-helvetica font-bold lg:text-[50px] text-[35px] lg:leading-[68px] text-whitetext'><span className='text-bordercoloryello'>Th</span>e Art of speed food Quality</h1>
