2a97c2b4b9b23794244bf18c7955d720a1a0dfe7
unknown <bshyder123@gmail.com>
2025-01-24 21:23:14 +0500

    Work Done

--- scripts/import-data.mjs
+++ scripts/import-data.mjs
@@ -7,10 +7,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../.env.local') });
 const client = createClient({
-  projectId: "nv1b21sg",
-  dataset: "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   useCdn: false,
-  token:"skDCJtEgFbrM5mZBJi4uGegzCMXYS3D1TthHtbW03uE7O7B19IY82WG7UxdrukqESF3FcLkLYPLhchSkR5ZzG2cZY4gDrAQ597QPLpF1XN3xI70l2eu95wc031rEU2nNfX6V6oaNdszxlVtpLiVZbUvM9QTnuyZn0OlWymXi8SbKp3xdfUyI",
+  token: process.env.SANITY_TOKEN,
   apiVersion: '2021-08-31',
 });
 async function uploadImageToSanity(imageUrl) {
